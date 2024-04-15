3523e7da4616b5fd3352f116042f437f25d7513a
Alexandra-Dolgih <dolgihas04@yandex.ru>
2024-04-15 21:49:17 +0500

    Шаблонизируй то

--- src/mock/destination.js
+++ src/mock/destination.js
@@ -1,13 +1,11 @@
 import { DESCRIPTION } from '../consts';
 
 const IMG_COUNT = 5;
-
 const BOUND = 20;
 
 function getMockDestination(identity) {
   return {
     id: identity,
-    city: CITY.get(identity),
     description: DESCRIPTION.get(identity),
     img: Array.from({ length: IMG_COUNT },() => `https://loremflickr.com/248/152?random=${Math.floor(Math.random() * BOUND)}`)
   };

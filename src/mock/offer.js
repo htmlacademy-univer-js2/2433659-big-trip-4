171cd0c4d83b4914d83ac11f2938c9d5b46a9785
Alexandra-Dolgih <dolgihas04@yandex.ru>
2024-06-18 22:24:21 +0500

    Финал

--- src/mock/offer.js
+++
@@ -1,8 +0,0 @@
-import {getRandomInteger, getRandomArrayElement} from './util.js';
-import{Price, OFFERS} from './const.js';
-
-export const getRandomOffer = () => ({
-  id:crypto.randomUUID(),
-  title: getRandomArrayElement(OFFERS),
-  price: getRandomInteger(Price.MIN, (Price.MAX / 10))
-});
